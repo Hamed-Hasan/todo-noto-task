95abf15949044ade0859f1acd39f6adb2b2d5aec
John Doe <hamedhasan422421@gmail.com>
2022-07-01 14:48:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2tv0rsp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('running test')
+})
+
+app.listen(port, () => {
+    console.log("Listening to port", port);
+})
